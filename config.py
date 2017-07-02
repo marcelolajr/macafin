a8234b0787a224d8a59991ec417144908b5b9dd9
xzjkya <xzjkya@gmail.com>
2017-07-01 23:39:06 -0300

    Adding environment configuration file.

---
+++ config.py
@@ -0,0 +1,73 @@
+"""
+    Configuration enviroments
+"""
+
+
+class Development():
+    """
+        Configuration used for development status
+    """
+
+    DEBUG = True
+    # TESTING = None
+    # PROPAGATE_EXCEPTIONS = None
+    # PRESERVE_CONTEXT_ON_EXCEPTION = None
+    # SECRET_KEY = None
+    # SESSION_COOKIE_NAME = None
+    # SESSION_COOKIE_DOMAIN = None
+    # SESSION_COOKIE_PATH = None
+    # SESSION_COOKIE_HTTPONLY = None
+    # SESSION_COOKIE_SECURE = None
+    # PERMANENT_SESSION_LIFETIME = None
+    # SESSION_REFRESH_EACH_REQUEST = None
+    # USE_X_SENDFILE = None
+    # LOGGER_NAME = None
+    # LOGGER_HANDLER_POLICY = None
+    # SERVER_NAME = None
+    # APPLICATION_ROOT = None
+    # MAX_CONTENT_LENGTH = None
+    # SEND_FILE_MAX_AGE_DEFAULT = None
+    # TRAP_HTTP_EXCEPTIONS = None
+    # TRAP_BAD_REQUEST_ERRORS = None
+    # PREFERRED_URL_SCHEME = None
+    # JSON_AS_ASCII = None
+    # JSON_SORT_KEYS = None
+    # JSONIFY_PRETTYPRINT_REGULAR = None
+    # SONIFY_MIMETYPE = None
+    TEMPLATES_AUTO_RELOAD = True
+    # EXPLAIN_TEMPLATE_LOADING = None
+
+
+class Production():
+    """
+        Configuration used for production status
+    """
+
+    DEBUG = True
+    # TESTING = None
+    # PROPAGATE_EXCEPTIONS = None
+    # PRESERVE_CONTEXT_ON_EXCEPTION = None
+    # SECRET_KEY = None
+    # SESSION_COOKIE_NAME = None
+    # SESSION_COOKIE_DOMAIN = None
+    # SESSION_COOKIE_PATH = None
+    # SESSION_COOKIE_HTTPONLY = None
+    # SESSION_COOKIE_SECURE = None
+    # PERMANENT_SESSION_LIFETIME = None
+    # SESSION_REFRESH_EACH_REQUEST = None
+    # USE_X_SENDFILE = None
+    # LOGGER_NAME = None
+    # LOGGER_HANDLER_POLICY = None
+    # SERVER_NAME = None
+    # APPLICATION_ROOT = None
+    # MAX_CONTENT_LENGTH = None
+    # SEND_FILE_MAX_AGE_DEFAULT = None
+    # TRAP_HTTP_EXCEPTIONS = None
+    # TRAP_BAD_REQUEST_ERRORS = None
+    # PREFERRED_URL_SCHEME = None
+    # JSON_AS_ASCII = None
+    # JSON_SORT_KEYS = None
+    # JSONIFY_PRETTYPRINT_REGULAR = None
+    # SONIFY_MIMETYPE = None
+    TEMPLATES_AUTO_RELOAD = True
+    # EXPLAIN_TEMPLATE_LOADING = None
