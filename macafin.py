f8bcc973a7ea2258be931ec2750cc186023963ed
xzjkya <xzjkya@gmail.com>
2017-07-19 19:43:34 -0300

    Adding extra routes.

--- macafin.py
+++ macafin.py
@@ -16,7 +16,7 @@ app.config.from_object(config.Development)
 # app.config.from_object(config.Production)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     """
         Index view handler
@@ -24,14 +24,54 @@ def index():
     return flask.render_template('index.html')
 
 
-@app.route('/report', methods=['POST'])
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    """
+        Dashboard view handler
+    """
+    return flask.render_template('dashboard.html')
+
+
+@app.route('/report', methods=['GET'])
 def report():
+    """
+        Report view handler
+    """
+    return flask.render_template('report.html')
+
+
+@app.route('/report_generation', methods=['POST'])
+def report_generation():
     """
         Report generation handler
     """
     params = flask.request.form
     report = cash_basis_generation(params['start'], params['end'], app)
     return flask.render_template('cash_basis.html', object=report)
+
+
+@app.route('/settings', methods=['GET'])
+def settings():
+    """
+        Settings view handler
+    """
+    return flask.render_template('settings.html')
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    """
+        About view handler
+    """
+    return flask.render_template('about.html')
+
+
+@app.route('/legal_information', methods=['GET'])
+def legal_information():
+    """
+        Legal information generation handler
+    """
+    return flask.render_template('legal_information.html')
 
 
 @app.errorhandler(404)
